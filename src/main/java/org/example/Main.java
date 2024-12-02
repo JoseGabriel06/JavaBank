477c1959244399123719786b3e63be6d2dca7a35
Gabriel Boch <gabo.boch96@gmail.com>
2024-12-02 11:40:44 -0600

    Agregado el contendio adicional del proyecto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Hola bro");
+      System.out.println("Hola bro, ahora voy a tener un parrafo bien fregon");
     }
 }
