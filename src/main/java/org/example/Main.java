555a5eedc53703deba76d1146caa2e5ce7791df0
Gabriel Boch <gabo.boch96@gmail.com>
2024-12-02 11:24:13 -0600

    Finalizada la cabecera pero mas bonita

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Soy una cabecera wey");
+        System.out.println("Soy una cabecera wey, pero ahora bonita");
         System.out.println("Hola bro");
     }
 }
