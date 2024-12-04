19af59d2062de3325f8eb052b6d433f5657bdfda
Gabriel Boch <gabo.boch96@gmail.com>
2024-12-04 11:45:06 -0600

    Agrego un error para corregir despues

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Soy una cabecera wey, pero ahora bonita");
+        System.out.println("Soy una cabeera wey, pero ahora bonita");
         System.out.println("Hola bro, ahora voy a tener un parrafo bien fregon");
     }
 }   
