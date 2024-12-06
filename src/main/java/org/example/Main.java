bdb3222c2dc1a35ac03a4ab79c19f04af332c861
Gabriel Boch <gabo.boch96@gmail.com>
2024-12-05 18:32:00 -0600

    Ex 1
    # Conflicts:
    #	src/main/java/org/example/Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Soy una cabecera wey, pero ahora un experimento");
+        System.out.println("Hola bro, ahora voy a tener un parrafo bien fregon");
         System.out.println("Soy una cabecera wey, pero ahora bonita");
         System.out.println("Hola bro, ahora voy a tener un parrafo bien fregon jaja");
     }
